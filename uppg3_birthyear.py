c23a74c69431d4075d1e1538212e23dc9575fada
RebeckaTei24A <rebkar080219@student.jenseneducation.se>
2025-08-28 09:52:55 +0200

    u3

--- uppg3_birthyear.py
+++ uppg3_birthyear.py
@@ -6,3 +6,7 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Du är (16) år gammal.
 
 """
+
+age = int(input("Vilket år är du född?"))
+
+print(f"Du är {2025-age} år gammal år 2025.")
